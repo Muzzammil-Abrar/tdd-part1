352c3c4653a199b9f666118c2eb0af43e58d45f9
Muzzammil-Abrar <macerace120@gmail.com>
2014-12-26 19:38:15 +0500

    First test has written.
    First test has written with compilation errors.

---
+++ src/moneyExample/MoneyExample.java
@@ -0,0 +1,15 @@
+package moneyExample;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class MoneyExample {
+
+	@Test
+	public void testMultiplication() {
+		Dollar five= new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+	}
+
+}

c5cc64c915a105cea30d330974e2d0a09e9a63c8
Muzzammil-Abrar <macerace120@gmail.com>
2014-12-29 12:45:04 +0500

    Comparing Dollar's with Dollar's.
    Dollar.times() returns an object of Dollar, so we can compare Dollar with Dollar because we want to make amount variable private.

--- src/moneyExample/MoneyExample.java
+++ src/moneyExample/MoneyExample.java
@@ -8,10 +8,10 @@ public class MoneyExample {
 	@Test
 	public void testMultiplication() {
 		Dollar five= new Dollar(5);
-		Dollar product= five.times(2);
-		assertEquals(10, product.amount);
-		product= five.times(3);
-		assertEquals(15, product.amount);
+		Dollar product = five.times(2);
+		assertEquals(new Dollar(10), product);
+		product = five.times(3);
+		assertEquals(new Dollar(15), product);
 	}
 	
 	@Test

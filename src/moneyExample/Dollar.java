f5ec44a6222b4152ba10d32f60455e2a095a2edb
Muzzammil-Abrar <Muzzammil-Abrar@users.noreply.github.com>
2015-01-02 21:08:04 +0500

    Chapter No. 9 : Added Currency
    Chapter No. 9 : Added Currency in money and initialized with USD in factory method of Dollad and with CHF in factory method of franc, both factory methods are in Money class.

--- src/moneyExample/Dollar.java
+++ src/moneyExample/Dollar.java
@@ -2,12 +2,16 @@ package moneyExample;
 
 public class Dollar extends Money {
 	
-	Dollar(int amount) {
-		this.amount = amount;
+	
+	Dollar(int amount , String currency) {
+		super(amount, currency);
 	}
 	
-	Dollar times(int multiplier) {
-		return new Dollar(amount * multiplier);
+	Money times(int multiplier) {
+		return Money.dollar(amount * multiplier);
 	}
-	
+	@Override
+	String currency(){
+		return currency;
+	}
 }

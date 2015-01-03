3f1a77e75bdb1f8112ba6eebb719afb34e55a94f
Muzzammil-Abrar <Muzzammil-Abrar@users.noreply.github.com>
2015-01-03 15:54:11 +0500

    Chapter No. 10 : Removed times() from Dollar
    Chapter No. 10 : Removed times() from Dollar

--- src/moneyExample/Dollar.java
+++ src/moneyExample/Dollar.java
@@ -6,12 +6,4 @@ public class Dollar extends Money {
 	Dollar(int amount , String currency) {
 		super(amount, currency);
 	}
-	
-	Money times(int multiplier) {
-		return Money.dollar(amount * multiplier);
-	}
-	@Override
-	String currency(){
-		return currency;
-	}
 }

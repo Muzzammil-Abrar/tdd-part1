098a32c7db8f98d70ce4f835c3a2a43feb32e4ff
Muzzammil-Abrar <Muzzammil-Abrar@users.noreply.github.com>
2015-01-03 18:26:37 +0500

    Chapter No. 14 : Added bank parameter.
    Chapter No. 14 : Added bank parameter in reduce() method..

--- src/moneyExample/Money.java
+++ src/moneyExample/Money.java
@@ -33,7 +33,8 @@ public class  Money implements Expression {
 	Expression plus(Money addend) {
 		return new Sum(this, addend);
 	}
-	public Money reduce(String to) {
-		return this;
-	}
+	public Money reduce(Bank bank, String to) {
+		int rate = bank.rate(currency, to);
+		return new Money(amount / rate, to);
+		}
 }
